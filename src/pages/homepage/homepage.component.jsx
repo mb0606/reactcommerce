470568f2eb0b807c0e5a8675a0360b67d266a910
mb0606 <marcoberardini@gmail.com>
2020-05-20 17:42:47 -0700

    Adding Directory components and associated styles

--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -1,13 +1,11 @@
 import React from 'react'
 import './homepage.styles.scss'
-import MenuItem from '../../components/menu-item/menu-item.components'
+import Directory from '../../components/directory/directory.component'
 
 const HomePage = () => {
   return (
     <div className="homepage">
-      <div className="directory-menu">
-        <MenuItem title="HATS" />
-      </div>
+      <Directory />
     </div>
   )
 }
